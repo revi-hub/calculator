aa8ffe407a30ec404f6925716679fb06311db51f
Revi <150064623+revi-hub@users.noreply.github.com>
2024-05-02 22:34:42 +0200

    Add divide function

--- app.js
+++ app.js
@@ -21,3 +21,17 @@ function multiply(a, b) {
     else
         return 'ERROR'
 }
+
+function divide(a, b) {
+    let intA = parseInt(a), intB = parseInt(b);
+    if(!isNaN(intA) &&  !isNaN(intB)){
+        if(intA == 0 && intB == 0)
+            return 'Result is undefined';
+        else if(intB == 0)
+            return 'Cannot divide by zero';
+        else
+            return intA / intB;
+    }
+    else
+        return 'ERROR'
+}
